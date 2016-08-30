79c0cd23813fffd193afb51d0e6a2531eeb3faf9
sala401 <sala401@SALA401_WS12.IUSH.LOCAL>
2016-08-30 07:14:34 -0500

    30 de agosto cambio

--- src/main/java/co/lab/aplicaciones/proyectoholel/main.java
+++ src/main/java/co/lab/aplicaciones/proyectoholel/main.java
@@ -12,6 +12,7 @@ package co.lab.aplicaciones.proyectoholel;
 public class main {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        System.out.println("Hola mundo");
     }
 
 }
